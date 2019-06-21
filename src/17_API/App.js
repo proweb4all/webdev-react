36080f69e1cb7a4b15ad8086e9e796ec35225fba
proweb4all <proweb4all@narod.ru>
2019-06-21 13:10:24 +0300

    17 API

---
+++ src/17_API/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+//import Lesson from './lesson';
+import News from './containers/news/news';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <News />
+      </div>
+    );
+  }
+}
